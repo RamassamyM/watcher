fbd32cdf620fdaa6c8f9afc9dc7231693ec54996
michael.ramassamy <michael.ramassamy@poietis.com>
2018-10-05 15:00:50 +0200

    creating queue system and log system

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# $(bash /home/michael.ramassamy/Bureau/Documents pro/code/00_scripts/watch_parse_send_data/log $0 "this is a test log message")
+$(./log $0 "this is a test log message")
